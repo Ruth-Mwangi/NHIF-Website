42ba0cd8c17b9c2c89c2b5e8fddb3a7f883ecd2c
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-03-04 13:33:39 +0300

    added ajax to jquery

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $("#reviewForm").submit(function(e){
+        e.preventDefault();
+
+        $.ajax({
+
+            url:"connect.php",
+            method:'post',
+            data:$("#reviewForm").serialize(),
+           success:function(data){
+               alert(data);
+           },error: function() {
+            alert('There was some error performing the AJAX call!');
+          }
+        });
+       
+
+    });
+
+    //signup
+    
+
+});
